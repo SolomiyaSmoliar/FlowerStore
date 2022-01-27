2e058aa06b911743f5ea0d01eb125d3df2c399d6
SolomiyaSmoliar <solomiia.smoliar@ucu.edu.ua>
2022-01-27 18:51:43 +0200

    initial commit

---
+++ src/main/java/delivery/PostDeliveryStrategy.java
@@ -0,0 +1,16 @@
+package delivery;
+
+import flowerstore.Item;
+import java.util.List;
+
+public class PostDeliveryStrategy implements Delivery{
+    @Override
+    public String delivery(List<Item> items) {
+        String order = "Your order: ";
+        for (int i = 0; i < items.size(); i++){
+            order += items.get(i).getString() + "\n";
+        }
+        order += "Order will be delivered by post.";
+        return order;
+    }
+}
