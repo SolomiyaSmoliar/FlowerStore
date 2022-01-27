b1df27771b7326a390ea06d97f2b7efd342baae0
SolomiyaSmoliar <solomiia.smoliar@ucu.edu.ua>
2022-01-27 18:46:54 +0200

    initial commit

---
+++ src/main/java/Decorators/ItemDecorator.java
@@ -0,0 +1,23 @@
+package Decorators;
+import flowerstore.Item;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+public class ItemDecorator implements Item {
+    protected Item item;
+
+    public ItemDecorator(Item item) {
+        this.item = item;
+    }
+
+    @Override
+    public double getPrice() {
+        return item.getPrice();
+    }
+
+    @Override
+    public String getString() {
+        return item.getString();
+    }
+}
